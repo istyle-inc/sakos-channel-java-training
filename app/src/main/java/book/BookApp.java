fa2097878404824bf613a2a458ac6980d20292fc
kamiyamak <kamiyamak@istyle.co.jp>
2022-08-29 18:52:30 +0900

    メソッド削除

--- app/src/main/java/book/BookApp.java
+++ app/src/main/java/book/BookApp.java
@@ -12,40 +12,11 @@ public class BookApp {
      * @return
      */
     public List<Book> シチュエーション1_本のリストに重複なく本を足したい(List<Book> books, Book... toBeAdded) {
-        var result = removeDuplication(books);
-        var additions = removeDuplication(Arrays.asList(toBeAdded));
-        var actualAdditions = new ArrayList<Book>();
-        
-        for (Book candidate : additions) {
-            if (checkNotDuplication(books, candidate)) {
-                if (checkNotDuplication(actualAdditions, candidate)) {
-                    actualAdditions.add(candidate);
-                }
-            }
-        }
-        result.addAll(actualAdditions);
-        return result;
-    }
+        var bookShelf = new BookShelf();
 
-    private List<Book> removeDuplication(List<Book> books) {
-        var bookList = new ArrayList<Book>();
-        
-        for (Book book : books) {
-            if (checkNotDuplication(bookList, book)) {
-                bookList.add(book);
-            }
-        }
-        return bookList;
-    }
-    
-    private boolean checkNotDuplication(List<Book> books, Book candidate) {
-        var foundDuplicationInList = false;
-        for (Book alreadyInList : books) {
-            if (alreadyInList.sameAs(candidate)) {
-                foundDuplicationInList = true;
-                break;
-            }
-        }
-        return !foundDuplicationInList;
+        books.forEach(book -> bookShelf.addBook(book));
+        List.of(toBeAdded).forEach(book -> bookShelf.addBook(book));
+
+        return bookShelf.bookList();
     }
 }
