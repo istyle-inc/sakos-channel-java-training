9bcbd5dac449a4827ba889803db87116395cdab6
kamiyamak <kamiyamak@istyle.co.jp>
2022-10-11 16:02:23 +0900

    20221011

--- app/src/main/java/date/BusinessDate.java
+++ app/src/main/java/date/BusinessDate.java
@@ -1,11 +1,13 @@
 package date;
 
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class BusinessDate {
     private final OffsetDateTime businessDate ;
-    public BusinessDate(OffsetDateTime businessDate) {
-        this.businessDate = businessDate;
+    public BusinessDate(Date date) {
+        this.businessDate = OffsetDateTime.ofInstant(date.toInstant(), ZoneId.of("Asia/Tokyo"));
     }
 
     public String getBusinessDateString() {
--- app/src/test/java/date/BusinessDateTest.java
+++ app/src/test/java/date/BusinessDateTest.java
@@ -1,16 +1,24 @@
 package date;
 
+import java.time.OffsetDateTime;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
+import java.util.Date;
+
+
 public class BusinessDateTest {
+    // @Test
+    // public void 営業日時を文字列で取得できること() {
+    //     var sut = new BusinessDate(OffsetDateTime.now());
+    //     var dateString = sut.getBusinessDateString();
+    //     assertThat(dateString)
+    //         .as("とりあえず出力させてみる")
+    //         .isEqualTo("???");
+    //         // (dateString instanceof String, String.class);
+    // }
     @Test
-    public void 営業日時を文字列で取得できること() {
-        var sut = new BusinessDate(OffsetDateTime.now());
-        var dateString = sut.getBusinessDateString();
-        assertThat(dateString instanceof String, String.class);
-    }
     public void 営業中かどうか確認() {
-        var sut = new BusinessDate(OffsetDateTime.now());
-        assertThat(sut.isInBusiness(), true);
+        var sut = new BusinessDate(new Date());
+        assertThat(sut.isInBusiness()).isEqualTo(true);
     }
 }
