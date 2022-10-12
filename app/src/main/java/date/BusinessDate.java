b0011dad2b1a2c0024d6f75c724702ec1ca411df
nagais-work <nagais@istyle.co.jp>
2022-10-12 12:31:02 +0900

    とりあえず動くように

--- app/src/main/java/date/BusinessDate.java
+++ app/src/main/java/date/BusinessDate.java
@@ -2,19 +2,49 @@ package date;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class BusinessDate {
     private final OffsetDateTime businessDate ;
+    private static final String FORMAT_PATTERN = "yyyy年MM月dd日";
+    private static final int REAL_DATE_MIDNIGHT = 0;
+    private static final int BUSINESS_DATE_MIDNIGHT = 4;
+    private static final int BUSINESS_TIME_START_HOUR = 9;
+    private static final int BUSINESS_TIME_END_HOUR = 18;
+
     public BusinessDate(Date date) {
-        this.businessDate = OffsetDateTime.ofInstant(date.toInstant(), ZoneId.of("Asia/Tokyo"));
+        var businessDate = OffsetDateTime.ofInstant(date.toInstant(), ZoneId.of("Asia/Tokyo"));
+        businessDate = this.roundedUpOfSecond(businessDate);
+        businessDate = this.AdjustmentDate(businessDate);
+
+        this.businessDate = businessDate;
+    }
+
+
+    public OffsetDateTime roundedUpOfSecond(OffsetDateTime date) {
+        if (date.getSecond() > date.truncatedTo(ChronoUnit.MINUTES).getSecond()) {
+            return date.plusMinutes(1);
+        }
+        return date;
+    }
+
+    public OffsetDateTime AdjustmentDate(OffsetDateTime date) {
+        var targetHour = date.getHour();
+        // 深夜0時から4時までの間のみ日付調整を行う
+        if (REAL_DATE_MIDNIGHT <= targetHour && targetHour < BUSINESS_DATE_MIDNIGHT) {
+            return date.plusDays(-1);
+        }
+        return date;
     }
 
     public String getBusinessDateString() {
-        return OffsetDateTime.now().toString();
+        return this.businessDate.format(DateTimeFormatter.ofPattern(FORMAT_PATTERN));
     }
 
     public boolean isInBusiness() {
-        return true;
+        var hour = this.businessDate.getHour();
+        return (BUSINESS_TIME_START_HOUR <= hour && hour < BUSINESS_TIME_END_HOUR);
     }
 }
--- app/src/test/java/date/BusinessDateTest.java
+++ app/src/test/java/date/BusinessDateTest.java
@@ -3,22 +3,112 @@ package date;
 import java.time.OffsetDateTime;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
+
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
 
 
 public class BusinessDateTest {
-    // @Test
-    // public void 営業日時を文字列で取得できること() {
-    //     var sut = new BusinessDate(OffsetDateTime.now());
-    //     var dateString = sut.getBusinessDateString();
-    //     assertThat(dateString)
-    //         .as("とりあえず出力させてみる")
-    //         .isEqualTo("???");
-    //         // (dateString instanceof String, String.class);
-    // }
     @Test
-    public void 営業中かどうか確認() {
-        var sut = new BusinessDate(new Date());
+    public void 営業日付を文字列で取得できる() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 7, 10, 0, 0);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.getBusinessDateString()).isEqualTo("2022年10月07日");
+    }
+
+    @Test
+    public void 営業日付を文字列で取得できる_JSTの時刻を渡す() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 7, 10, 0, 0);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.getBusinessDateString()).isEqualTo("2022年10月07日");
+    }
+
+    @Test
+    public void 営業日付を文字列で取得できる_UCTの時刻を渡しても日本時間で取得できる() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        testDate.set(2022, 9, 7, 20, 0, 0);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.getBusinessDateString()).isEqualTo("2022年10月08日");
+    }
+
+    @Test
+    public void 営業日付を文字列で取得できる_境界値_2022年10月7日3時59分00秒は10月6日() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 7, 3, 59, 0);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.getBusinessDateString()).isEqualTo("2022年10月06日");
+    }
+
+    @Test
+    public void 営業日付を文字列で取得できる_境界値_2022年10月7日3時59分01秒は10月7日() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 7, 3, 59, 1);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.getBusinessDateString()).isEqualTo("2022年10月07日");
+    }
+
+    @Test
+    public void 営業日付を文字列で取得できる_2022年10月6日23時59分0秒は10月6日() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 6, 23, 59, 0);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.getBusinessDateString()).isEqualTo("2022年10月06日");
+    }
+
+    @Test
+    public void 営業日付を文字列で取得できる_2022年10月7日0時1分00秒は10月6日() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 7, 0, 1, 0);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.getBusinessDateString()).isEqualTo("2022年10月06日");
+    }
+
+    @Test
+    public void 営業中かどうか確認＿営業時間中() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 7, 10, 0, 0);
+        var sut = new BusinessDate(testDate.getTime());
         assertThat(sut.isInBusiness()).isEqualTo(true);
+    }
+    @Test
+    public void 営業中かどうか確認_営業時間外() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 7, 20, 0, 0);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.isInBusiness()).isEqualTo(false);
+    }
+    @Test
+    public void 営業中かどうか確認_2022年10月7日8時59分00秒_営業前である() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 7, 3, 59, 0);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.isInBusiness()).isEqualTo(false);
+    }
+    @Test
+    public void 営業中かどうか確認_2022年10月7日8時59分01秒_営業中である() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 7, 8, 59, 1);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.isInBusiness()).isEqualTo(true);
+    }
+    @Test
+    public void 営業中かどうか確認_2022年10月7日17時59分00秒_営業中である() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 7, 17, 59, 0);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.isInBusiness()).isEqualTo(true);
+    }
+    @Test
+    public void 営業中かどうか確認_2022年10月7日17時59分01秒_営業終了中である() {
+        var testDate = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+        testDate.set(2022, 9, 7, 17, 59, 1);
+        var sut = new BusinessDate(testDate.getTime());
+        assertThat(sut.isInBusiness()).isEqualTo(false);
     }
 }
